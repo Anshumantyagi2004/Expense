a0371793ddbceacc081502904bc38cff713ac807
Anshumantyagi2004 <161432160+Anshumantyagi2004@users.noreply.github.com>
2026-02-07 17:01:53 +0530

    origin final fix

--- Back-end/index.js
+++ Back-end/index.js
@@ -13,8 +13,8 @@ const PORT = 5000;
 
 app.use(
   cors({
-    origin: "*",
-    // origin: "https://expense-q8e5.vercel.app/",
+    // origin: "http://localhost:5173",
+    origin: "https://expense-q8e5.vercel.app",
     credentials: true,
   })
 );
